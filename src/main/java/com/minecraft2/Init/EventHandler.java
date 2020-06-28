8cfca3149ca715f4c68da6a5d6f9ec74a441252f
Daniel Abdi <danielabdi32@gmail.com>
2020-06-27 23:52:28 -0400

    Troubleshooting EventHandler

--- src/main/java/com/minecraft2/Init/EventHandler.java
+++ src/main/java/com/minecraft2/Init/EventHandler.java
@@ -3,6 +3,7 @@ package com.minecraft2.Init;
 import com.minecraft2.ClairoSpawnItem;
 import com.minecraft2.minecraft2mod;
 import net.minecraft.block.Blocks;
+import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.util.Hand;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -10,13 +11,17 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 public class EventHandler {
     @SubscribeEvent
     public void interact(PlayerInteractEvent event) {
-        if (event.getWorld().isRemote) {
+        if (!event.getWorld().isRemote) {
             if (event.getHand() == Hand.MAIN_HAND) {
                 minecraft2mod.logger.info("INTERACTED");
+                //minecraft2mod.logger.info(event.getSide());
                 if (event.getWorld().getBlockState(event.getPos()).getBlock() == Blocks.OAK_DOOR) {
-                    if (event.getPlayer().inventory.hasItemStack(ItemInit.bag.getDefaultInstance()) && !event.getPlayer().inventory.hasItemStack(minecraft2mod.RegistryEvents.clairo_egg.getDefaultInstance())) {
-                        event.getPlayer().inventory.addItemStackToInventory(minecraft2mod.RegistryEvents.clairo_egg.getDefaultInstance());
-                        event.getPlayer().inventory.removeStackFromSlot(event.getPlayer().inventory.getSlotFor(ItemInit.bag.getDefaultInstance()));
+                    ServerPlayerEntity player = (ServerPlayerEntity) event.getPlayer();
+
+                    if (player.inventory.hasItemStack(ItemInit.bag.getDefaultInstance()) && !event.getPlayer().inventory.hasItemStack(minecraft2mod.RegistryEvents.clairo_egg.getDefaultInstance())) {
+                        player.inventory.addItemStackToInventory(minecraft2mod.RegistryEvents.clairo_egg.getDefaultInstance());
+                        player.inventory.removeStackFromSlot(event.getPlayer().inventory.getSlotFor(ItemInit.bag.getDefaultInstance()));
+
                     }
                 }
             }
